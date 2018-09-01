538c8e4c2e2605402e7f3d25728aeaab8a63a436
Maximillian Naza <maximillianaza@gmail.com>
2018-09-01 08:02:44 -0400

    Make form page default home page

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const app = express();
 app.set('view engine', 'pug');
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get('/', function(request, response) {
+	return response.redirect('/form');
+});
+
 app.get('/form', function(request, response) {
 	return response.render('form');
 });
